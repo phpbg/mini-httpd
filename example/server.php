d67824077fd9976a7ab92e371f30e484bd0510ff
phpbg <chemla.samuel@gmail.com>
2018-10-06 23:08:28 +0200

    more doc

--- example/server.php
+++ example/server.php
@@ -2,10 +2,14 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+/**
+ * This is a sample tasks management class.
+ */
 class TasksController extends \PhpBg\MiniHttpd\Controller\AbstractController
 {
     use \PhpBg\MiniHttpd\Middleware\ContextTrait;
 
+    // Initial tasks
     public $tasks = ['task1', 'task2'];
 
     /**
@@ -62,27 +66,51 @@ $routes = [
     '/api/task/add-async' => new \PhpBg\MiniHttpd\Model\Route([$taskController, 'addAsync'], $jsonRenderer),
 ];
 
+// Application context will be injected in a request attribute.
+// See \PhpBg\MiniHttpd\Middleware\ContextTrait to retrieve it easily
 $applicationContext = new \PhpBg\MiniHttpd\Model\ApplicationContext();
+
+// You may require loop for async tasks
 $applicationContext->loop = $loop;
-$applicationContext->options = [
-    'this' => 'is where',
-    'you can store' => 'configuration directives'];
+
+// You can put your configuration directves in options
+$applicationContext->options = [];
 $applicationContext->routes = $routes;
+
+// Public path is where static files are served from
 $applicationContext->publicPath = __DIR__ . '/public';
+
+// You can share your PSR3 logger here
 $applicationContext->logger = new \PhpBg\MiniHttpd\Logger\Console(\Psr\Log\LogLevel::DEBUG);
 
 $server = new \React\Http\Server([
+    // Log all incoming requests
     new \PhpBg\MiniHttpd\Middleware\LogRequest($applicationContext->logger),
+
+    // Make application context and request context available to all middlewares. Allow data exchanging between middlewares
     new \PhpBg\MiniHttpd\Middleware\Context($applicationContext),
+
+    // Decode once uri path
     new \PhpBg\MiniHttpd\Middleware\UriPath(),
+
+    // Serve static files
     new \PhpBg\MiniHttpd\Middleware\StaticContent($applicationContext->publicPath),
+
+    // Prepare fore rendering
     new \PhpBg\MiniHttpd\Middleware\Render($jsonRenderer),
+
+    // Log exceptions
     new \PhpBg\MiniHttpd\Middleware\LogError($applicationContext->logger),
+
+    // Calculate route
     new \PhpBg\MiniHttpd\Middleware\Route(),
+
+    // Run route selected
     new \PhpBg\MiniHttpd\Middleware\Run()
 ]);
 
+// Run server on port 8080
+// just open your browser and go to http://localhost:8080
 $socket = new React\Socket\Server(8080, $loop);
 $server->listen($socket);
-
 $loop->run();
